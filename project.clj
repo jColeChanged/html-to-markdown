838d08548a6a4873324b7a46a7719e627441d7ac
Joshua <jColeChanged@gmail.com>
2011-03-27 23:27:28 -0700

    Removed unused dependency. Prepared to add the project to clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject html-to-markdown "1.0.0-SNAPSHOT"
+(defproject org.clojars.joshua/html-to-markdown "1.0.0-SNAPSHOT"
   :description "Converts HTML to markdown."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [clojure-saxon "0.9.1-SNAPSHOT"]]
-   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
